5bafe2772f36df14c64bdc12d706f2e05b69522e
ellipticcurv3 <14904229+ellipticcurv3@users.noreply.github.com>
2019-12-14 00:48:44 +0100

    This is better, I hope

--- .travis/merge-into-live.sh
+++ .travis/merge-into-live.sh
@@ -7,15 +7,10 @@ git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 git fetch --all || exit
 
 temporaryDirectory="$(mktemp -d)";
-cp -r ./dist/ "$temporaryDirectory/";
-
-
+cp -rT ./dist/ "$temporaryDirectory/";
+cd "$temporaryDirectory";
+git init
 git checkout --orphan live || exit
-git rm --cached -r .
-rm -r ./* ./.*
-cp -rT "$temporaryDirectory/dist/" ./
 git add --all
 git commit -m "Travis CI $TRAVIS_COMMIT"
-git push
-
-#git push https://${GITHUB_TOKEN}@github.com/ellipticcurv3/www.git
+git push --set-upstream https://${GITHUB_TOKEN}@github.com/ellipticcurv3/www.git live
