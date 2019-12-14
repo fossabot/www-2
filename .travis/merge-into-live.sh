248c1447837e2504fe98aed90c45569f473f565c
ellipticcurv3 <14904229+ellipticcurv3@users.noreply.github.com>
2019-12-14 01:10:24 +0100

    Another try

--- .travis/merge-into-live.sh
+++ .travis/merge-into-live.sh
@@ -3,15 +3,18 @@
 export GIT_COMMITTER_EMAIL="travis@travis"
 export GIT_COMMITTER_NAME="Travis CI"
 
-git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
-git fetch --all || exit
+#git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
+#git fetch --all || exit
 
-temporaryDirectory="$(mktemp -d)";
-cp -rT ./dist/ "$temporaryDirectory/";
-cd "$temporaryDirectory";
+temporaryDirectory1="$(mktemp -d)";
+temporaryDirectory2="$(mktemp -d)";
+cp -rT ./dist/ "$temporaryDirectory1/";
+cd "$temporaryDirectory1";
 rm .placeholder
-git init
-git checkout --orphan live || exit
+cd "$temporaryDirectory2";
+git clone --depth 10 https://github.com/ellipticcurv3/www.git .
+git checkout live
+cp -rT "$temporaryDirectory1/" "$temporaryDirectory2/"
 git add --all
 git commit -m "Travis CI $TRAVIS_COMMIT"
 git push --set-upstream https://${GITHUB_TOKEN}@github.com/ellipticcurv3/www.git live
